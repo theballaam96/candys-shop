172e9bb16b1d64b847b727aa25bf7b588b0e4acc
Tom Ballaam <theballaam96@gmail.com>
2024-02-18 12:04:29 -0600

    Test

---
+++ .github/scripts/pr-comment-post.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+const { Octokit } = require("@octokit/rest");
+
+async function run() {
+    try {
+      // PR Data
+      const prNumber = process.env.PR_NUMBER;
+      const repo = "theballaam96/candys-shop";
+      const token = process.env.GITHUB_TOKEN;
+      const response = await axios.get(`https://api.github.com/repos/${repo}/pulls/${prNumber}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+  
+      // Extract the PR message
+      let user = "Unknown";
+      if (response.data.user) {
+          user = response.data.user.login;
+      }
+      let content = `New PR Comment: ${process.env.PR_URL}. PR Author: ${user}`;
+      const webhookUrl = process.env.DISCORD_WEBHOOK_PRCOMMENT;
+      const options = {
+          method: "POST",
+          url: webhookUrl,
+          headers: { "Content-Type": "application/json" },
+          data: {
+              content: content
+          },
+      }
+      axios(options)
+          .then(whresp => {
+              console.log('Message sent successfully:', whresp.data);
+          })
+          .catch(error => {
+              console.log(error.message);
+              process.exit(1);
+          });
+    } catch (error) {
+      console.error('Error:', error.response ? error.response.data : error.message || error);
+      process.exit(1);
+    }
+  }
+  
+  run();
